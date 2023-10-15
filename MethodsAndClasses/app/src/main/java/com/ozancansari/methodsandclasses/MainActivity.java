215f920860bcc613f3fc9276b606387f7b276ba9
7ozzy35 <84852769+7ozzy35@users.noreply.github.com>
2023-10-16 00:58:32 +0300

    keke

---
+++ MethodsAndClasses/app/src/main/java/com/ozancansari/methodsandclasses/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ozancansari.methodsandclasses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello keke");
+    }
+
+}
+
